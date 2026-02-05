f772ef8122188c3e7d8c30ba0a4e79c898d3eb90
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2026-02-05 18:12:35 -0500

    Finished from-scratch Problems 1-4 and Short Response

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,13 +1,28 @@
 const resolvedWrapper = (value) => {
+  return Promise.resolve(value)
 };
 
 const rejectedWrapper = (errorMessage) => {
+  return Promise.reject(new Error(errorMessage));
 };
 
 const handleResolvedPromise = (promise) => {
+  return promise.then((value) => {
+    console.log(value);
+    return value.toUpperCase();
+  });
 };
 
 const handleResolvedOrRejectedPromise = (promise) => {
+  return promise
+  .then((value) => {
+    console.log(value);
+    return value.toUpperCase();
+  })
+  .catch((error) => {
+    console.error(`Your error message was: ${error.message}`)
+    return null
+  });
 };
 
 const pauseForMs = (ms) => {
